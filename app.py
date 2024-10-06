613ccf0128e50ea9a4dd5dc9e7842b8e4ff0f89d
Bingo's Code <sabareesh.kappagantu@gmail.com>
2024-10-05 23:07:08 -0700

    Added Ideogram interface to get the responses from the generate api

--- app.py
+++ app.py
@@ -62,9 +62,6 @@ def get_latest_user_message(message_history):
             return message['content']
     return None
 
-
-
-
 @traceable
 @cl.on_chat_start
 def on_chat_start():    
--- text_to_image_interface.py
+++ text_to_image_interface.py
@@ -5,7 +5,8 @@ import os
 
 load_dotenv()
 
-generation_endpoint = os.getenv("IDEOGRAM_ENDPOINT") + "/generation"
+generation_endpoint = os.getenv("IDEOGRAM_ENDPOINT") + "/generate"
+
 def get_storybook_illustration(title, characters, cover_picture_description, num_pages, pages):
     print("Generating storybook illustration...")
     print(f"Title: {title}")
@@ -15,7 +16,20 @@ def get_storybook_illustration(title, characters, cover_picture_description, num
     print(f"Pages: {pages}")
     print("Storybook illustration generated successfully!")
 
-    COVER_PICTURE_PROMPT = f"Cover picture for {title} with {characters} and following the description: {cover_picture_description}"
+    all_character_features = ""
+    for character in characters:
+        character_name = character.character_name
+        traits = character.character_traits
+        features = character.character_features
+        all_character_features += f"Name of {character_name}:\nTraits of {traits}:\nFeatures of {features}:\n"
+
+
+    COVER_PICTURE_PROMPT = f"""Generate a cover picture for a children's story book using the following prompt: {cover_picture_description}
+        Below you will find a description of each characters that you may need to use for the cover picture based on the above description.
+        {all_character_features}
+    """
+    print("\n--->DEBUG: ", COVER_PICTURE_PROMPT)
+
     headers = {
         "Api-Key": f"{os.getenv("IDEOGRAM_API_KEY")}",
         "Content-Type": "application/json"
@@ -23,7 +37,7 @@ def get_storybook_illustration(title, characters, cover_picture_description, num
     
     payload = { 
         "image_request": {
-            "prompt": f"{COVER_PICTURE_PROMPT}",
+            "prompt": f"{cover_picture_description}",
             "aspect_ratio": "ASPECT_10_16",
             "model": "V_2",
             "magic_prompt_option": "AUTO"
@@ -36,3 +50,5 @@ def get_storybook_illustration(title, characters, cover_picture_description, num
 
     return response.json()
     
+
+    #https://ideogram.ai/assets/image/lossless/response/H78te17kQ62VMi9F9RnwPQ
